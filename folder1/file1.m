d6865867c7e1b9dfc99819f89560906266055093
Igor Kagan <igor.b.kagan@gmail.com>
2015-08-07 18:57:38 +0200

    added header template
    added header template - what's more to say?

--- folder1/file1.m
+++ folder1/file1.m
@@ -1,1 +1,32 @@
-% file 1
+function out = file1(in)
+%functionname  - one line function description
+%
+% USAGE:
+% usage example1;
+% usage example2;
+%		...
+%
+% INPUTS:
+%		input 1		- exlanation
+%		...
+%
+% OUTPUTS:
+%		output1		- explanation
+%		...
+%
+% REQUIRES:	...
+%
+% See also ...
+%
+%
+% Author(s):	I.Kagan, DAG, DPZ
+% URL:		http://www.dpz.eu/dag
+%
+% Change log:
+% yyyymmdd:	Created function (Author(s) firstname familyname)
+% ...
+% $Revision: 1.0 $  $Date: 2015-08-07 18:48:27 $
+
+% ADDITIONAL INFO:
+% ...
+%%%%%%%%%%%%%%%%%%%%%%%%%[DAG mfile header version 1]%%%%%%%%%%%%%%%%%%%%%%%%% 
